f669fe1dc27c8a7072c46abb383b692d1e6752ab
Abby Rosenthal <adlundeen@gmail.com>
2022-09-15 19:43:51 -0500

    some projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function Project({ project }) {
-const {name, description, image, deployedLink, repo } = project;
+    const { name, description, image, deployedLink, repo } = project;
+
+    return (
+        <div key={name}>
+            <p className="project-name">{name} | <a href={deployedLink}>Live Project</a> | <a href={repo}>GitHub Repo</a></p>
+            <img className="project-picture" src={require(`../../assets/Placeholder.png`)} alt={name} />
+        </div>
+    )
 
 }
+
